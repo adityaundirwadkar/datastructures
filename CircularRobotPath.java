e119afc2e59bdf683244691796a7992f0b2a566f
Aditya Undirwadkar <aditya.undirwadkar@gmail.com>
2015-04-25 19:53:45 -0500

    Update CircularRobotPath.java

--- CircularRobotPath.java
+++ CircularRobotPath.java
@@ -1,3 +1,19 @@
+/*
+Given a sequence of moves for a robot, check if the sequence is circular or not. A sequence of moves is circular if first and last positions of robot are same. A move can be on of the following.
+
+  G - Go one unit
+  L - Turn left
+  R - Turn right 
+
+Examples:
+
+Input: path[] = "GLGLGLG"
+Output: Given sequence of moves is circular 
+
+Input: path[] = "GLLG"
+Output: Given sequence of moves is circular 
+
+*/
 
 public class CircularRobotPath {
 
